136aa5eb37ce23940f51f805645d553e75eec72d
Inal Gotov <sketch204@gmail.com>
2023-11-28 18:59:32 -0500

    Add more options to `RemoteImage`

--- Sources/RemoteImage/RemoteImage.swift
+++ Sources/RemoteImage/RemoteImage.swift
@@ -1,24 +1,45 @@
 import SwiftUI
 
-struct RemoteImage: View {
-    let url: URL
-    let provider: RemoteImageProvider
+public struct RemoteImage<ImageContent, PlaceholderContent>: View where ImageContent: View, PlaceholderContent: View {
+    private let url: URL
+    private let provider: RemoteImageProvider
+    
+    private let imageContent: (Image) -> ImageContent
+    private let placeholderContent: PlaceholderContent
     
     public init(
         _ url: URL,
-        provider: RemoteImageProvider = .shared
+        provider: RemoteImageProvider = .shared,
+        @ViewBuilder imageContent: @escaping (Image) -> ImageContent,
+        placeholderContent: PlaceholderContent = Image(systemName: "photo")
     ) {
         self.url = url
         self.provider = provider
+        self.imageContent = imageContent
+        self.placeholderContent = placeholderContent
     }
     
-    var body: some View {
+    public var body: some View {
         RemoteImageLoaderView(url, provider: provider) {
-            ProgressView()
+            placeholderContent
         } successContents: { image in
-            image
+            imageContent(image)
         } failureContents: { error in
-            Image(systemName: "exclamationmark.triangle")
+            placeholderContent
         }
+    }
+}
+
+extension RemoteImage {
+    public init(
+        _ url: URL,
+        provider: RemoteImageProvider = .shared,
+        @ViewBuilder imageContent: @escaping (Image) -> ImageContent,
+        @ViewBuilder placeholderContent: () -> PlaceholderContent
+    ) {
+        self.url = url
+        self.provider = provider
+        self.imageContent = imageContent
+        self.placeholderContent = placeholderContent()
     }
 }
